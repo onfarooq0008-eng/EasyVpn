bdf38b8c86bc1ec5bedfdf5b85e79fcbe02b309c
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-08-29 21:26:38 +0400

    Delete fixui directory

--- fixui/app/proguard-rules.pro
+++
@@ -1,6 +0,0 @@
-# WireGuard / Jni
--keep class com.wireguard.** { *; }
--keepclassmembers class com.wireguard.** { *; }
-# Gson models
--keep class com.easyvpn.app.data.** { *; }
--keepattributes Signature
--- fixui/app/src/main/java/com/easyvpn/app/ui/HomeRow.kt
+++
@@ -1,8 +0,0 @@
-package com.easyvpn.app.ui
-
-import com.easyvpn.app.data.Server
-
-sealed class HomeRow {
-    data class Header(val group: CountryGroup, val expanded: Boolean) : HomeRow()
-    data class ServerRow(val server: Server) : HomeRow()
-}
